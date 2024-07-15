7646fd3cdd2542bc63e10bb823e09cef25cd5d80
oeta <oeta.kumara@gmail.com>
2024-07-15 14:35:07 +0200

    - added example xml files - first attempt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            // Specify the path to the XML file
+            String filePath = "./src/main/resources/REQ_01.xml";
+
+            // Read the file into a string
+            String content = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
+
+            // Print the content
+            System.out.println(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
